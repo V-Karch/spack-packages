142552d4dea36800393bb72b5daeeac7c73f6590
V-Karch <179492080+V-Karch@users.noreply.github.com>
2026-09-22 15:37:54 +0000

    [@spackbot] updating style on behalf of V-Karch

--- repos/spack_repo/builtin/packages/py_kraken_biom/package.py
+++ repos/spack_repo/builtin/packages/py_kraken_biom/package.py
@@ -16,9 +16,7 @@ class PyKrakenBiom(PythonPackage):
 
     license("MIT", checked_by="V-Karch")
 
-    version(
-        "1.2", sha256="64b37c06800c768616b1a27d44c0216e14140fedecef7a8800286f97f56bfc68"
-    )
+    version("1.2", sha256="64b37c06800c768616b1a27d44c0216e14140fedecef7a8800286f97f56bfc68")
 
     depends_on("py-setuptools", type="build")
     depends_on("py-biom-format@2.1.5:", type=("build", "run"))
